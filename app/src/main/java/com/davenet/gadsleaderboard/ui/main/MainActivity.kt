1467885784465f67e471da9bea3126cda0531c7a
shazomii <seyeoyeniran2u@gmail.com>
2020-09-18 12:57:46 +0100

    Switch to viewPager2

--- app/src/main/java/com/davenet/gadsleaderboard/ui/main/MainActivity.kt
+++ app/src/main/java/com/davenet/gadsleaderboard/ui/main/MainActivity.kt
@@ -2,12 +2,16 @@ package com.davenet.gadsleaderboard.ui.main
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import com.davenet.gadsleaderboard.R
 import com.davenet.gadsleaderboard.ui.submission.SubmitActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_text_1,
+    R.string.tab_text_2
+)
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +19,20 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
+        viewpager.adapter = SectionsPagerAdapter(lifecycle, supportFragmentManager)
+        TabLayoutMediator(
+            tabs, viewpager
+        ) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = getString(TAB_TITLES[0])
+                }
+                1 -> {
+                    tab.text = getString((TAB_TITLES[1]))
+                }
+            }
+        }.attach()
+
         submitButton.setOnClickListener {
             startActivity(Intent(this, SubmitActivity::class.java))
         }
--- app/src/main/java/com/davenet/gadsleaderboard/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/davenet/gadsleaderboard/ui/main/SectionsPagerAdapter.kt
@@ -1,36 +1,26 @@
 package com.davenet.gadsleaderboard.ui.main
 
-import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.davenet.gadsleaderboard.R
-
-private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
-)
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
-    : FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(lifecycle: Lifecycle, fm: FragmentManager) :
+    FragmentStateAdapter(fm, lifecycle) {
 
-    override fun getItem(position: Int): Fragment {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> FragmentHours()
             else -> FragmentSkills()
         }
-    }
-
-    override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
-    }
-
-    override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
     }
 }
